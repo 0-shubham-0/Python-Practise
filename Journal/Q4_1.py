75ed239b2bff889111f42a0ac43d88454c0e3d7d
Shubham Talawadekar <89774595+0-shubham-0@users.noreply.github.com>
2022-04-27 16:20:38 +0530

    .

--- Journal/Q4_1.py
+++ Journal/Q4_1.py
@@ -1,12 +1,12 @@
-import sys
+my_list = [3, 7, 9, 4, 6]
+print(my_list)
+i = int(input('Enter Index : '))
+try:
+    print(my_list[i])
+except IndexError as e:
+    print(e)
 
-ch = 0
-while ch != 2:
-    ch = int(input('1.Input\t\t2.Exit'))
-    if ch == 1:
-        i = int(input('Enter Index : '))
-        try:
-            my_list = [3, 7, 9, 4, 6]
-            print(my_list[i])
-        except IndexError as e:
-            print(e)
+try:
+    print(0/0)
+except Exception as e:
+    print(e)
