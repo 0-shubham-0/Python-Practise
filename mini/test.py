b602a6da6bca0e125d8ecc3310740ec6c161f22e
Shubham Talawadekar <89774595+0-shubham-0@users.noreply.github.com>
2022-04-30 16:54:23 +0530

    .

--- mini/test.py
+++ mini/test.py
@@ -3,16 +3,35 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
 
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.b1 = None
+        self.label = None
+        self.setGeometry(200, 200, 500, 300)
+        self.setWindowTitle("test")
+        self.initUI()
+
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("my Label")
+        self.label.move(50, 50)
+
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.move(50, 70)
+        self.b1.setText("button")
+        self.b1.clicked.connect(self.clicks)
+
+    def clicks(self):
+        self.label.setText("Button Clcickedawdawd fawf")
+        self.update()
+
+    def update(self):
+        self.label.adjustSize()
+
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200, 200, 300, 300)
-    win.setWindowTitle("test")
-
-    label = QtWidgets.QLabel(win)
-    label.setText("my Label")
-    label.move(50, 50)
-
+    win = MyWindow()
 
     win.show()
     sys.exit(app.exec_())
