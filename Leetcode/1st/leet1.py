08f24b7fd720715643d9a2716ca518629fbcb471
Shubham Talawadekar <89774595+0-shubham-0@users.noreply.github.com>
2022-11-12 17:31:45 +0530

    leet in python

---
+++ Leetcode/1st/leet1.py
@@ -0,0 +1,24 @@
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    for i in range(len(list)):
+        for j in range(i+1,index):
+            if list[i]+list[j]==target:
+                return [i,j]
+
+
+
+# print(twoSum([1,2,3],5))
+
+
+list=[1,23,45,0]
+target=23
+index=len(list)
+print(twoSum(list,target))
+# for i in range(len(list)):
+#     for j in range(i+1,index):
+#         if list[i]+list[j]==target:
+#             print(i,j)
