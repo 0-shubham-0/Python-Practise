945173680ff840d5f74a3f96fb40e5a818abe10a
Shubham Talawadekar <89774595+0-shubham-0@users.noreply.github.com>
2023-03-19 14:38:02 +0530

    .

--- Leetcode/leet1472.py
+++ Leetcode/leet1472.py
@@ -1,4 +1,7 @@
-class BrowserHistory:
+# 1472. Design Browser History
+# Difficulty: Medium
+# Solved using list
+class BrowserHistory: 
 
     def __init__(self, homepage: str):
         self.list=[homepage]
@@ -32,3 +35,36 @@ class BrowserHistory:
             return self.list[self.indx]
         self.indx=self.indx+steps
         return self.list[self.indx]
+
+
+# Solved using Doubly Linked List
+class TreeNode:
+    def __init__(self, url, next=None, prev=None):
+        self.url=url
+        self.next=next
+        self.prev=prev
+
+
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.curr= TreeNode(homepage)
+
+
+    def visit(self, url: str) -> None:
+        self.curr.next = TreeNode(url,prev=self.curr)
+        self.curr= self.curr.next
+        
+
+    def back(self, steps: int) -> str:
+        while self.curr.prev and steps>0:
+            self.curr= self.curr.prev
+            steps-=1
+        return self.curr.url
+
+
+    def forward(self, steps: int) -> str:
+        while self.curr.next and steps>0:
+            self.curr= self.curr.next
+            steps-=1
+        return self.curr.url
